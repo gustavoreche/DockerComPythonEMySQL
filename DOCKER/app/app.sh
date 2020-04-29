4ebebec1ea4d28d9ca3373408320438584eade32
Gustavo Dolmen Reche <gustavodreche@msn.com>
2020-04-29 09:39:32 -0300

    Atualiza projeto do docker
    Atualiza projeto do docker

--- DOCKER/app/app.sh
+++ DOCKER/app/app.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-pip install bottle==0.12.13 mysql-connector-python==8.0.13
+pip install --upgrade pip
+pip install bottle==0.12.13 mysql-connector-python==8.0.13 redis==2.10.5
 python -u enviador.py
--- DOCKER/app/enviador.py
+++ DOCKER/app/enviador.py
@@ -1,31 +1,43 @@
 import mysql.connector
-from bottle import route, run, request
+import redis
+import json
+from bottle import Bottle, request
 from datetime import date
 
-def registraMensagem(assunto, mensagem):
-    conexao = mysql.connector.connect(host="db", user="root", passwd="root", database="emailDocker")
-    cursor = conexao.cursor()
-    sql = 'INSERT INTO emails (data_email, assunto, mensagem) VALUES (%s, %s, %s)'
-    dataAtual = date.today()
-    dataFormatada = dataAtual.strftime('%Y/%m/%d')
-    valores = (dataFormatada, assunto, mensagem)
-    cursor.execute(sql, valores)
-    cursor.close()
-    conexao.commit()
-    conexao.close()
+class Enviador(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.envia)
+        self.fila = redis.StrictRedis(host='filaService', port=6379, db=0)
+        self.conn = mysql.connector.connect(host="db", user="root", passwd="root", database="emailDocker")
 
-    print('Mensagem registrada!')
+    def registraMensagem(self, assunto, mensagem):
+        #conexao = mysql.connector.connect(host="db", user="root", passwd="root", database="emailDocker")
+        cursor = self.conn.cursor()
+        sql = 'INSERT INTO emails (data_email, assunto, mensagem) VALUES (%s, %s, %s)'
+        dataAtual = date.today()
+        dataFormatada = dataAtual.strftime('%Y/%m/%d')
+        valores = (dataFormatada, assunto, mensagem)
+        cursor.execute(sql, valores)
+        cursor.close()
+        self.conn.commit()
+        #self.conn.close()
 
-@route('/', method="POST")
-def envia():
-    assunto = request.forms.get('assunto')
-    mensagem = request.forms.get('mensagem')
+        mensagem = {'assunto': assunto, 'mensagem': mensagem}
+        self.fila.rpush('enviador', json.dumps(mensagem))
 
-    registraMensagem(assunto, mensagem)
+        print('Mensagem registrada!')
 
-    return 'Mensagem na fila! <br> Assunto: {}. <br> Mensagem: {}.'.format(
-        assunto, mensagem
-    )
+    def envia(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+
+        self.registraMensagem(assunto, mensagem)
+
+        return 'Mensagem na fila! <br> Assunto: {}. <br> Mensagem: {}.'.format(
+            assunto, mensagem
+        )
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port = 8080, debug = True)
+    enviador = Enviador()
+    enviador.run(host='0.0.0.0', port = 8080, debug = True)
